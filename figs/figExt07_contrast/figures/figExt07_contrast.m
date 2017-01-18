8cc9c28b6d8a6827573a9aebbc173b1ba32268b0
Ugurcan Mugan <umugan@u.northwestern.edu>
2017-01-17 19:30:34 -0600

    DOI added and bug images moved to data folder

--- figs/figExt07_contrast/figures/figExt07_contrast.m
+++ figs/figExt07_contrast/figures/figExt07_contrast.m
@@ -5,6 +5,10 @@ function figExt07_contrast
 %% Title                : A massive increase in visual range preceded the origin of terrestrial vertebrates
 %% Authors              : Ugurcan Mugan, Malcolm A. MacIver
 %% Authors' Affiliation : Northwestern University
+%% DOI for code: 10.5281/zenodo.239228
+%% This work is licensed under the Creative Commons Attribution 4.0 International License. 
+%% To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
+%% January 2017
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Initialize variables 
 global BIGEYEROOT
